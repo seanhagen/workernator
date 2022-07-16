8f99893d536f2f125bbd9bb55fd90e1bb060a79d
Sean Patrick Hagen <sean.hagen@gmail.com>
2022-07-15 18:27:50 -0700

    Simplify the tests since I got the non-binary version to work
    Had to do some futzing about with `TestMain`, but everything seems to be working
    now.

--- magefiles/testing.go
+++ magefiles/testing.go
@@ -20,35 +20,12 @@ func Test() error {
 }
 
 func runSubtests(pkg string) error {
-	fmt.Printf("creating %v test binary...", pkg)
-
-	bin := fmt.Sprintf("%v.test", pkg)
-
-	err := sh.Run("go", "test", "-c", fmt.Sprintf("./%v", pkg))
+	fmt.Printf("Running tests for '%v'...", pkg)
+	err := sh.Run("go", "test", fmt.Sprintf("./%v", pkg))
 	if err != nil {
-		fmt.Println(" ERROR")
-		return fmt.Errorf("unable to create '%v' test binary:\n\t%w", pkg, err)
-	}
-	fmt.Printf(" DONE: generated test binary '%v'\n", bin)
-
-	fmt.Printf("Running %v tests...", pkg)
-	if err := sh.Run(fmt.Sprintf("./%v", bin), "-test.v"); err != nil {
 		fmt.Printf(" ERROR\n")
-		fmt.Printf("removing test binary '%v'...", bin)
-		if e2 := sh.Rm(bin); e2 != nil {
-			fmt.Printf("ERROR: unable to remove '%v' test binary, %v", bin, e2)
-		}
-		fmt.Println(" DONE")
-
 		return err
 	}
-	fmt.Println(" DONE")
-
-	fmt.Printf("cleaning up after tests...")
-	if err := sh.Rm(bin); err != nil {
-		fmt.Printf(" ERROR: %v", err)
-	}
-	fmt.Println(" DONE")
-
+	fmt.Printf(" DONE\n")
 	return nil
 }
